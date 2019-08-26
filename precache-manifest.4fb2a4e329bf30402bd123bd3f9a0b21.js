f661ea2a8e99b2df52f9cf5395d7ea693ae85966
Angnima Sherpa <angnimasherpa7@gmail.com>
2019-08-26 17:45:17 +0545

    deploy

---
+++ precache-manifest.4fb2a4e329bf30402bd123bd3f9a0b21.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3650a12e7acebdc6ab8c",
+    "url": "/js/product.c37a5609.js"
+  },
+  {
+    "revision": "f426dc2781760b6c5472",
+    "url": "/js/chunk-vendors.36c79ba3.js"
+  },
+  {
+    "revision": "b5b10c32059bdb981a5c",
+    "url": "/js/app.4560d7cc.js"
+  },
+  {
+    "revision": "2d7f461513de13700d20",
+    "url": "/js/about.a5145b48.js"
+  },
+  {
+    "revision": "3eaf0457b5eaa9993d07e9406c6e7785",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3650a12e7acebdc6ab8c",
+    "url": "/css/product.b5447fd5.css"
+  },
+  {
+    "revision": "f426dc2781760b6c5472",
+    "url": "/css/chunk-vendors.e3fda5c0.css"
+  },
+  {
+    "revision": "b5b10c32059bdb981a5c",
+    "url": "/css/app.7c451db8.css"
+  },
+  {
+    "revision": "2d7f461513de13700d20",
+    "url": "/css/about.390f8165.css"
+  }
+];
