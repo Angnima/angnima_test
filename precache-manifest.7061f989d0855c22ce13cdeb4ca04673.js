48e98cf86e0a5f467b27ef200916e0a0d2db9490
Angnima <angnimasherpa7@gmail.com>
2019-08-26 14:52:20 +0545

    For deploy
    yes

---
+++ precache-manifest.7061f989d0855c22ce13cdeb4ca04673.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/<test_app>/robots.txt"
+  },
+  {
+    "revision": "b744ace337bfaf7691e2",
+    "url": "/<test_app>/js/chunk-vendors.b9bf9b67.js"
+  },
+  {
+    "revision": "d728a1b766aaabb94ebc",
+    "url": "/<test_app>/js/app.b6d43c67.js"
+  },
+  {
+    "revision": "a84b453e6614eeda6982",
+    "url": "/<test_app>/js/about.66de3a7f.js"
+  },
+  {
+    "revision": "98177d03583ba9e6d79edf04e7b895af",
+    "url": "/<test_app>/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/<test_app>/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d728a1b766aaabb94ebc",
+    "url": "/<test_app>/css/app.b7b29f14.css"
+  }
+];
